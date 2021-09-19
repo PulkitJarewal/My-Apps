05de509817b4d0630f9a02e36cc3f77cf0ddcaa3
PulkitJarewal <pulkitjarewal01@gmail.com>
2021-09-19 12:53:10 +0530

    Hello User

---
+++ app/src/main/java/com/example/comessage/ContactMode.java
@@ -0,0 +1,24 @@
+package com.example.comessage;
+
+public class ContactMode {
+    String name, number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getNumber() {
+
+        return number;
+    }
+
+    public void setNumber(String number)
+    {
+        this.number = number;
+    }
+}
